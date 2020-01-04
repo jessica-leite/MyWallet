3a8af30eb6062ff4c9bb9f41fa342144c7f49d3b
Jessica <jessica@email.com>
2020-01-04 14:59:34 +0000

    SB-Admin-2 template, Layout, BundleConfig and PartialViews added

--- MyWallet.Web/App_Start/BundleConfig.cs
+++ MyWallet.Web/App_Start/BundleConfig.cs
@@ -8,23 +8,17 @@ namespace MyWallet.Web
         // For more information on bundling, visit https://go.microsoft.com/fwlink/?LinkId=301862
         public static void RegisterBundles(BundleCollection bundles)
         {
-            bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                        "~/Scripts/jquery-{version}.js"));
-
-            bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
-                        "~/Scripts/jquery.validate*"));
-
-            // Use the development version of Modernizr to develop with and learn from. Then, when you're
-            // ready for production, use the build tool at https://modernizr.com to pick only the tests you need.
-            bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
-                        "~/Scripts/modernizr-*"));
-
-            bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
-                      "~/Scripts/bootstrap.js"));
+            bundles.Add(new StyleBundle("~/Content/fontawesome").Include(
+                      "~/vendor/fontawesome-free/css/all.min.css"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
+                     "~/css/sb-admin-2.min.css"));
+
+            bundles.Add(new ScriptBundle("~/bundles/jquery-bootstrap").Include(
+                "~/vendor/jquery/jquery.min.js",
+                "~/vendor/bootstrap/js/bootstrap.bundle.min.js",
+                "~/vendor/jquery-easing/jquery.easing.min.js",
+                "~/js/sb-admin-2.min.js"));
         }
     }
 }
