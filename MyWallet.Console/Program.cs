84ed21d6d351f7b17ba0e7b9769ba31d18f85d30
Jessica <jessica@email.com>
2020-01-05 20:52:56 +0000

    MyWallet.Console added and Entity Framework tested

---
+++ MyWallet.Console/Program.cs
@@ -0,0 +1,27 @@
+﻿using MyWallet.Data;
+using MyWallet.Data.Domain;
+using System;
+
+namespace MyWallet.Console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var context = new MyWalletDBContext())
+            {
+                string connectionString = context.Database.Connection.ConnectionString;
+
+                var user = new User();
+                user.Name = "Jéssica";
+                user.LastName = "Leite";
+                user.Email = "jessica@email.com";
+                user.Password = "123456";
+                user.CreationDate = DateTime.Now;
+
+                context.User.Add(user);
+                context.SaveChanges();
+            }
+        }
+    }
+}
