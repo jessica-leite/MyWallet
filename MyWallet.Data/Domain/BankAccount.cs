a2f1d0e3013bba2627757d07106dac3e12eb6564
Jessica <jessica@email.com>
2020-01-11 13:29:48 +0000

    BankAccount Added

---
+++ MyWallet.Data/Domain/BankAccount.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyWallet.Data.Domain
+{
+    public class BankAccount
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int UserId { get; set; }
+        public User User { get; set; }
+    }
+}
