1367769e32176da3045d62034c774e3ccafc3b53
Jessica <jessica@email.com>
2020-01-28 20:13:04 +0000

    Income - Controller, ViewModel and View

---
+++ MyWallet.Web/ViewModels/Income/IncomeViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace MyWallet.Web.ViewModels.Income
+{
+    public class IncomeViewModel
+    {
+        public int Id { get; set; }
+        public string Description { get; set; }
+        public decimal Value { get; set; }
+        public DateTime Date { get; set; }
+        public bool Received { get; set; }
+        public DateTime CreationDate { get; set; }
+        public string Observation { get; set; }
+        public int ContextId { get; set; }
+        public string Context { get; set; }
+        public int BankAccountId { get; set; }
+        public string BankAccount { get; set; }
+        public int CategoryId { get; set; }
+        public string Category { get; set; }
+    }
+}
---
+++ MyWallet.Web/ViewModels/Income/ListAllIncomeViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+
+namespace MyWallet.Web.ViewModels.Income
+{
+    public class ListAllIncomeViewModel
+    {
+        public List<IncomeViewModel> IncomeList { get; set; }
+        public string Currency { get; set; }
+
+        public ListAllIncomeViewModel()
+        {
+            IncomeList = new List<IncomeViewModel>();
+        }
+    }
+}
